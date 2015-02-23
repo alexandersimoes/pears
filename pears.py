15dc6d0ae21753b8b0d9f08e6960ee53b64461dd
Alex Simoes <alexandersimoes@gmail.com>
2015-02-22 19:43:20 -0500

    initial design concept

---
+++ pears.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+import os
+from flask import Flask, render_template, request, redirect, url_for, abort, session, send_from_directory
+
+app = Flask(__name__, template_folder="html")
+app.config['DEBUG'] = True
+app.config['UPLOAD_FOLDER'] = os.path.join(os.path.dirname(__file__), 'uploads')
+
+'''Access to static files'''
+@app.route('/uploads/<user>/<path:filename>')
+def uploaded_file(user, filename):
+    photo_dir = os.path.join(app.config['UPLOAD_FOLDER'], user)
+    ''' need to use this send_from_directory function so that the proper HTTP
+        headers are set and HTML audio API can work properly'''
+    return send_from_directory(photo_dir, filename)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/pears/')
+def pears():
+    return render_template('pears.html')
+
+@app.route('/toc/')
+def toc():
+    return render_template('toc.html')
+
+'''
+
+    Run the file!
+    
+'''
+if __name__ == '__main__':
+  app.run()
